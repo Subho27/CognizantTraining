27151f660bf55d45559e1dc6d17a785f411bb8de
Subhodip Rudra <subhodiprudra18@gmail.com>
2021-03-09 00:52:09 +0530

    Initial commit

---
+++ Asynchronous/NamedAndptional/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedAndptional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("COHORT DETAILS");
+            
+            GetCohortDetails("DF002", "22", "OBL", "DOTNET","Advanced Learnig");
+            GetCohortDetails("DF002", "OBL", "22" ,"DOTNET", "Advanced Learnig");
+            GetCohortDetails(CohortName:"DF002", mode:"OBL",currentModule: "Advanced Learnig", GencCount: "22", track:"DOTNET");
+
+            Console.WriteLine("ORDER DETAILS");
+            OrderDetails(seller:"SOME SELLER",product:"SOME PRODUCT");
+
+            Console.ReadKey();
+        }
+
+        public static void GetCohortDetails(string CohortName,string GencCount,string mode,string track,string currentModule)
+        {
+            Console.WriteLine($"It is {CohortName} with {GencCount} GenCs undergoing training for {track} thru {mode}. The current module of training is {currentModule}");
+        }
+        public static void OrderDetails(string seller,string product,int quantity=10,bool isReturnable=true)
+        {
+            Console.WriteLine($"Here is the order detail – {quantity} number of {product} by {seller} is ordered. It’s returnable status is {isReturnable}");
+        }
+    }
+}
---
+++ Threading/ThreadStartSample/ThreadStartSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadStartSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*ThreadStart Delegate App *\n");
+            Console.Write("Do you want [1] or [2] threads? ");
+            string threadCount = Console.ReadLine();
+
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+
+            Console.WriteLine("-> {0} is executing Main()",
+            Thread.CurrentThread.Name);
+
+            Printer p = new Printer();
+
+            switch (threadCount)
+            {
+                case "2":
+                    Thread backgroundThread = new Thread(new ThreadStart(p.PrintNumbers));
+                    backgroundThread.Name = "Secondary";
+                    backgroundThread.Start();
+                    break;
+                case "1":
+                    p.PrintNumbers();
+                    break;
+                default:
+                    Console.WriteLine("I dont know what...you get 1 thread.");
+                    goto case "1";
+            }
+            Console.WriteLine("Hello this from main!");
+        }
+    }
+}
