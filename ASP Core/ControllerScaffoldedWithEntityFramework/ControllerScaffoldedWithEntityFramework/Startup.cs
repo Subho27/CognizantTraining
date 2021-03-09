09605a3341c5ac78ffac4350d9353568dc8cedd9
Subhodip Rudra <subhodiprudra18@gmail.com>
2021-03-09 22:09:24 +0530

    Second Commit

--- ASP Core/ControllerScaffoldedWithEntityFramework/ControllerScaffoldedWithEntityFramework/Startup.cs
+++ ASP Core/ControllerScaffoldedWithEntityFramework/ControllerScaffoldedWithEntityFramework/Startup.cs
@@ -55,7 +55,7 @@ namespace ControllerScaffoldedWithEntityFramework
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Accounts}/{action=Index}/{id?}");
             });
         }
     }
